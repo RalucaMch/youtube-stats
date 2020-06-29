c5e363b0771b3c364715c483247b6d13244c62d9
RALUCA MECHNO <fortech.mechno@extaccount.com>
2020-06-29 17:08:39 +0300

    test

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header(){
   return(
-    <header className="mx-auto p-8 bg-red-700">
+    <header className="mx-auto p-8 bg-green-700">
       <h1 className="text-white text-4xl">YouTube Stats</h1>
     </header>
   )
